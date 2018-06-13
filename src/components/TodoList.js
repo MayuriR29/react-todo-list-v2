98560eb8931f3b95607d9cf4a9f176f81daeac05
MayuriR29 <mayuri.gr@gmail.com>
2018-06-13 17:49:07 +0800

    changes for todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,28 @@
-import {todos} from "../seedData"
-class TodoList{
-    
+import { todos } from "../seedData";
+import React, { Component } from 'react';
+import Title from "./Title"
+class TodoList extends Component {
+    constructor(){
+       super();
+       this.state = {
+          todos :todos
+       }
+    }
+  render() {
+    return (
+    <div className="TodoList">
+    <Title props="My todo list"/>
+    {this.state.todos.map((val,index)=>{
+        return(
+        <ul>
+            <li>{val.name} </li>
+        </ul>
+        )
+    })
+
+    }
+    </div>
+    );
+  }
 }
+export default TodoList
